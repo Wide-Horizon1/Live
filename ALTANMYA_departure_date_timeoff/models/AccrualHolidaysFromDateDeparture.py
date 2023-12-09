1f88d259c5acc2e477272e002327d7d7df6ac60f
ghaith1212 <142239135+ghaith1212@users.noreply.github.com>
2023-12-09 15:40:16 +0300

    Update AccrualHolidaysFromDateDeparture.py

--- ALTANMYA_departure_date_timeoff/models/AccrualHolidaysFromDateDeparture.py
+++ ALTANMYA_departure_date_timeoff/models/AccrualHolidaysFromDateDeparture.py
@@ -49,6 +49,10 @@ class AccrualHolidaysFromDateDeparture(models.Model):
                             employee.departure_date)
                         _logger.info("  mapped days in calc +++++++++++++")
                         _logger.info(mapped_days)
+                        _logger.info( allocation.holiday_status_id)
+                        _logger.info(holiday.employee_id)
+                        _logger.info((allocation.employee_id | allocation.employee_ids).ids)
+                        _logger.info(employee.departure_date)
                         if allocation.holiday_type != 'employee' \
                                 or not allocation.employee_id \
                                 or allocation.holiday_status_id.requires_allocation == 'no':
