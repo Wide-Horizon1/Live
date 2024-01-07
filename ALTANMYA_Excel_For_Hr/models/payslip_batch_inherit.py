1358617313118beaefe5b0b47dc309e1bb20550c
ghaith1212 <142239135+ghaith1212@users.noreply.github.com>
2024-01-07 13:43:13 +0300

    Update payslip_batch_inherit.py

--- ALTANMYA_Excel_For_Hr/models/payslip_batch_inherit.py
+++ ALTANMYA_Excel_For_Hr/models/payslip_batch_inherit.py
@@ -82,6 +82,8 @@ class HrPayslip(models.Model):
             _LOGGER.info("basiccccccccccccc haerererereerrererererer compute :")
             _LOGGER.info(rec)
             self.basic_sal= 0.0
+            self.mobile_allowance = 0.0
+
 
             for line in rec.worked_days_line_ids:
                 if line.work_entry_type_id.code == 'ATTEND' or line.work_entry_type_id.code == 'WORK100' :
