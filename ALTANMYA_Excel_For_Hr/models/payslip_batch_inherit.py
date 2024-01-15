1949583b82da78f5975bf32803958c8877692d57
ANASNADERALGENAIMI <142088414+ANASNADERALGENAIMI@users.noreply.github.com>
2024-01-15 10:13:02 +0300

    Add files via upload

--- ALTANMYA_Excel_For_Hr/models/payslip_batch_inherit.py
+++ ALTANMYA_Excel_For_Hr/models/payslip_batch_inherit.py
@@ -13,91 +13,38 @@ class HrPayslip(models.Model):
 
     emp_name=fields.Char(related='employee_id.name')
     emp_id=fields.Char(related='employee_id.identification_id')
-    house_wage=fields.Float(compute='_compute_net')
-    allowances=fields.Float(compute='_compute_net')
-    deductions =fields.Float(compute='_compute_net')
-    mobile_allowance =fields.Float(compute='_compute_net',default=0.0)
-    transportation_allowance =fields.Float(compute='_compute_net',default=0.0)
-    food_allowance =fields.Float(compute='_compute_net',default=0.0)
-    nature_of_work=fields.Float(compute='_compute_net',default=0.0)
-    total_allowances=fields.Float(compute='_compute_net',default=0.0)
-    other_allowances =fields.Float(compute='_compute_net',default=0.0)
-    busniess_trip =fields.Float(compute='_compute_net',default=0.0)
+    house_wage=fields.Float(compute='_compute_fields')
+    allowances=fields.Float(compute='_compute_fields')
+    deductions =fields.Float(compute='_compute_fields')
+    mobile_allowance =fields.Float(compute='_compute_fields',default=0.0)
+    transportation_allowance =fields.Float(compute='_compute_fields',default=0.0)
+    food_allowance =fields.Float(compute='_compute_fields',default=0.0)
+    nature_of_work=fields.Float(compute='_compute_fields',default=0.0)
+    total_allowances=fields.Float(compute='_compute_fields',default=0.0)
+    other_allowances =fields.Float(compute='_compute_fields',default=0.0)
+    busniess_trip =fields.Float(compute='_compute_fields',default=0.0)
     worked_days= fields.Float(string = "worked days" , compute='_compute_days',default=0.0)
-    basic_sal = fields.Float(string = "Baseic days" , compute='_compute_net')
-    rewards = fields.Float(string = "Rewards " , compute='_compute_net',default=0.0)
-    retrived = fields.Float(string = "Retrived " , compute='_compute_net',default=0.0)
+    basic_sal = fields.Float(string = "Baseic days" ,compute='_compute_fields')
+    rewards = fields.Float(string = "Rewards " , compute='_compute_fields',default=0.0)
+    retrived = fields.Float(string = "Retrived " , compute='_compute_fields',default=0.0)
     day_value = fields.Float(string = "Day value ",default=0.0)
     hour_cost = fields.Float(string = "Hour Cost",default=0.0)
-    additional_constant = fields.Float(string = "Rewards " , compute='_compute_net')
-    over_hours = fields.Float(string = "overtime hour",compute='_compute_net' )
-    over_days = fields.Float(string = "overtime days",compute='_compute_net' )
-    over_value = fields.Float(string = "overtime vlue", compute='_compute_net')
-    accrual_total  = fields.Float(string = "Accrual total  ", compute='_compute_net')
+    additional_constant = fields.Float(string = "Rewards " , compute='_compute_fields')
+    over_hours = fields.Float(string = "overtime hour",compute='_compute_fields' )
+    over_days = fields.Float(string = "overtime days",compute='_compute_fields' )
+    over_value = fields.Float(string = "overtime vlue", compute='_compute_fields')
+    accrual_total  = fields.Float(string = "Accrual total  ", compute='_compute_fields')
     total_for_bank = fields.Float(string="Total ")
-    insurance_discount = fields.Float(string = "Insurance Discount", compute='_compute_net')
-    training_discount = fields.Float(string = "Training Discount", compute='_compute_net')
-    traffic_fine_deduction = fields.Float(string = "Traffic Fine", compute='_compute_net')
-    aramco_lost = fields.Float(string = "Aramco ", compute='_compute_net')
-    advance_discount = fields.Float(string = "Advance Discount ", compute='_compute_net')
-    penalty_deduction = fields.Float(string = "Penalty deduction ", compute='_compute_net')
+    insurance_discount = fields.Float(string = "Insurance Discount", compute='_compute_fields')
+    training_discount = fields.Float(string = "Training Discount", compute='_compute_fields')
+    traffic_fine_deduction = fields.Float(string = "Traffic Fine", compute='_compute_fields')
+    aramco_lost = fields.Float(string = "Aramco ", compute='_compute_fields')
+    advance_discount = fields.Float(string = "Advance Discount ", compute='_compute_fields')
+    penalty_deduction = fields.Float(string = "Penalty deduction ", compute='_compute_fields')
     total_deduction = fields.Float(string="Total deduction")
 
-
-
-    def prepare_excel_data(self):
-        data = {
-            'emp_id': self.emp_id,
-            'emp_name': self.emp_name,
-            'house_wage': self.house_wage,
-            'allowances': self.allowances,
-            'deductions': self.deductions,
-            'mobile_allowance': self.mobile_allowance,
-            'transportation_allowance': self.transportation_allowance,
-            'food_allowance': self.food_allowance,
-            'nature_of_work': self.nature_of_work,
-            'other_allowances': self.other_allowances,
-            'worked_days': self.worked_days,
-            'basic_sal': self.basic_sal,
-            # Add more fields as needed
-        }
-        return data
-
-
-    def action_print_payslip_excel(self):
-        print("here ")
-        data = self.prepare_excel_data()
-
-        repor_action=self.env.ref("ALTANMYA_Excel_For_Hr.report_payroll_details_xls3")
-        return repor_action.report_action(docids=self.ids,data=data)
-
-
-
     @api.depends('name')
-    def _compute_days(self):
-        print("i am here ")
-        for rec in self:
-            
-            sum_days = 0.0
-            _LOGGER.info("basiccccccccccccc haerererereerrererererer compute :")
-            _LOGGER.info(rec)
-            _LOGGER.info(self)
-
-            for line in rec.worked_days_line_ids:
-                if line.work_entry_type_id.code == 'ATTEND' or line.work_entry_type_id.code == 'WORK100' :
-                    print("hello")
-                    sum_days+=line.number_of_days
-                    _LOGGER.info(sum_days)
-                    print("sum isss----- -", sum_days)
-            rec.worked_days= sum_days
-            print("work day ",sum_days, rec.worked_days )
-            _LOGGER.info("work day :::::::::::::::;" )
-            _LOGGER.info(sum_days )
-
-
-    @api.depends('name')
-    def _compute_net(self):
-        _LOGGER.info("basiccccccccccccc haerererereerrererererer neeeet :")
+    def _compute_fields(self):
         category_mapping = {
             'Allowance': 'allowances',
             'Deduction': 'deductions',
@@ -107,14 +54,14 @@ class HrPayslip(models.Model):
             'Food': 'food_allowance',
             'Nature': 'nature_of_work',
             'Other': 'other_allowances',
-            'Rewards':'rewards',
+            'Rewards': 'rewards',
             'Retrived': 'retrived',
             'Totalallowances': 'total_allowances',
             'BusinessTrip': 'busniess_trip',
             'FixedOvertime': 'additional_constant',
-            'OVT1':'over_value',
-            'OVTD':'over_days',
-            'OVTH':'over_hours',
+            'OVT1': 'over_value',
+            'OVTD': 'over_days',
+            'OVTH': 'over_hours',
             'Gosi': 'insurance_discount',
             'Training': 'training_discount',
             'TrafficFine': 'traffic_fine_deduction',
@@ -122,11 +69,19 @@ class HrPayslip(models.Model):
             'Advance': 'advance_discount',
             'Penalty': 'penalty_deduction',
         }
-        _LOGGER.info("\ddddddddddddddddddddddddddddddd haerererereerrererererer :")
-        for payslip in self:
+        for payslip in self :
+            print("record" ,payslip )
+
+            if not payslip.basic_sal :
+                payslip.basic_sal = payslip.basic_wage
+                payslip.day_value = payslip.basic_sal / 30
+                payslip.hour_cost =( payslip.basic_sal / 30 ) /8
+
+            else:
+                payslip.basic_sal = 0.0
+
+
             category_sums = {field: 0.0 for field in category_mapping.values()}
-            _LOGGER.info(" pay sip from haerererereerrererererer :",payslip)
-            _LOGGER.info(payslip)
 
             for line in payslip.line_ids:
                 category_name = line.category_id.name
@@ -136,38 +91,34 @@ class HrPayslip(models.Model):
 
             for worked_days_line in payslip.worked_days_line_ids:
                 workdays_name = worked_days_line.work_entry_type_id.code
-                # print("work days is ", worked_days_line , workdays_name)
-                # # Perform custom calculations for the specific categories
                 if workdays_name == 'OVT1':
                     print("2")
                     category_sums['over_days'] += worked_days_line.number_of_days
                     category_sums['over_hours'] += worked_days_line.number_of_hours
-                # elif workdays_name == 'OVTH':
-                #     print("3")
 
-                # if workdays_name == 'Attendance':
-                #     print("4")
-                #     category_sums['attendance'] += worked_days_line.number_of_days
 
             for field, value in category_sums.items():
-                # print("categories is ", category_sums)
                 setattr(payslip, field, value)
-            _LOGGER.info(payslip.basic_sal)   
-            if not payslip.basic_sal :
-                _LOGGER.info("payslip basiccc1111111111111111111111111c")
 
-                payslip.basic_sal = payslip.basic_wage
-                payslip.day_value = payslip.basic_sal / 30
-                payslip.hour_cost =( payslip.basic_sal / 30 ) /8
-                _LOGGER.info("payslip basicccc")
-                _LOGGER.info(payslip.basic_sal)   
+    def action_print_payslip_excel(self):
+        print("here ")
+        data = self.prepare_excel_data()
 
+        repor_action=self.env.ref("ALTANMYA_Excel_For_Hr.report_payroll_details_xls3")
+        return repor_action.report_action(docids=self.ids,data=data)
+    @api.depends('name')
+    def _compute_days(self):
+        print("i am here ")
+        for rec in self:
+            sum_days = 0.0
+            _LOGGER.info("basiccccccccccccc haerererereerrererererer compute :")
+            _LOGGER.info(rec)
+            _LOGGER.info(self)
+            for line in rec.worked_days_line_ids:
+                if line.work_entry_type_id.code == 'ATTEND' or line.work_entry_type_id.code == 'WORK100' :
+                    # print("i am here ")
+                    sum_days+=line.number_of_days
 
-            else:
-                payslip.basic_sal = 0.0
-
-
-
-
+            rec.worked_days= sum_days
 
 
